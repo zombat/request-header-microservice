4cc3b4cbde88f59d33d681c14bf4efdfe472ec46
Raymond <raymond.rizzo@gmail.com>
2017-04-05 17:31:56 -0400

    Added core files.

---
+++ requestHeader.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.get('/', function(request, response){
+    response.writeHead(200, { "Content-Type": "application/json" });
+    var theOS = request.headers['user-agent'].match(/\((.+\d)\)/);
+    theOS = theOS[0].substring(1,theOS[0].length-1);
+    var requestHeaders = { 
+        'ipaddress' : request.ip,
+        'language' : request.headers['accept-language'].split(',')[0],
+        'software' : theOS
+    }; 
+    response.end(JSON.stringify(requestHeaders));
+});
+
+http.createServer(app).listen(process.env.PORT);
